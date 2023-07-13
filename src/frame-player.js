e7739b5ef7dc7400a5e1baa3de061d94579740d1
Gabriela Payano <gabipayano@Gabrielas-MacBook-Air.local>
2023-07-12 21:15:51 -0700

    adding ff playlist

--- src/frame-player.js
+++ src/frame-player.js
@@ -3,8 +3,7 @@ import React from 'react';
 export default function Player(){
     return(
         <div id="player">
-            <iframe id="inlineFrameExample" title="Inline Frame Example" width="300" height="200" src="https://www.youtube.com//embed/PL6AREfkbkLFYBnO1eoqMnk0fzhsO42S6S">
-            </iframe>
+            <iframe id="inlineFrameExample" title="Inline Frame Example" width="300" height="200" src="https://www.youtube.com/embed/HeGVeBWECu8" />
       </div>
     )
 
