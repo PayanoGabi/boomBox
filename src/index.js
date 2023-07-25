ff1f6c745aedfeca8fa6b0bcadb7b1f6f4f73d57
Gabriela Payano <gabipayano@Gabrielas-MacBook-Air.local>
2023-07-24 18:10:31 -0700

    loading videos in request

--- src/index.js
+++ src/index.js
@@ -14,7 +14,18 @@ var requestOptions = {
 
 fetch("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=48&playlistId=PL6AREfkbkLFYBnO1eoqMnk0fzhsO42S6S&key=AIzaSyC2wnUh9-JdRSTisOOQ4j4LKr1MCYU65U4", requestOptions)
   .then(response => response.text())
-  .then(result => console.log(result))
+  .then(result => {
+    //console.log(result)
+    var data = JSON.parse(result).items;
+  
+    data.forEach(element => {
+      console.log(element.snippet.resourceId.videoId)
+      const vidId = element.snippet.resourceId.videoId
+      const srcString = "https://www.youtube.com/embed/" + vidId
+      
+    });
+    
+  })
   .catch(error => console.log('error', error));
 
   var tag = document.createElement('script');
@@ -23,4 +34,13 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+setTimeout(10000, checkit())
 
+
+var iframePlayer = document.querySelector("#player > iframe");
+console.log(iframePlayer)
+function checkit(){
+if(iframePlayer){
+  console.log('wazaa')
+}
+}
